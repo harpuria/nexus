67e11f52d6b0388f70d09440441df810832e1c34
YHH <harpuria87@gmail.com>
2025-06-26 17:09:40 +0900

    1. 공통 인터페이스 작성 - Result (성공 및 실패하는 경우에 응답용으로 사용할 공통 인터페이스)

---
+++ src/main/java/com/qwerty/nexus/global/Result.java
@@ -0,0 +1,41 @@
+package com.qwerty.nexus.global;
+
+/**
+ * 응답용으로 사용할 공통 Result interface
+ * @param <T>
+ */
+public sealed interface Result<T> permits Result.Success, Result.Failure {
+
+    /**
+     * 성공
+     * @param data
+     * @param message
+     * @param <T>
+     */
+    record Success<T>(T data, String message) implements Result<T> {
+        public static <T> Success<T> of(T data, String message) {
+            return new Success<>(data, message);
+        }
+
+        public static <T> Success<T> of(T data) {
+            return new Success<>(data, "성공");
+        }
+    }
+
+    /**
+     * 실패
+     * @param message
+     * @param errorCode
+     * @param cause
+     * @param <T>
+     */
+    record Failure<T>(String message, String errorCode, Exception cause) implements Result<T> {
+        public static <T> Failure<T> of(String message, String errorCode) {
+            return new Failure<>(message, errorCode, null);
+        }
+
+        public static <T> Failure<T> of(String message, String errorCode, Exception cause) {
+            return new Failure<>(message, errorCode, cause);
+        }
+    }
+}
