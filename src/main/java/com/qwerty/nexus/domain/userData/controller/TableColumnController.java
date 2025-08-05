5bdc1db88e1ec4f981c464a68742e82b1aced7a1
YHH <harpuria87@gmail.com>
2025-08-05 17:38:28 +0900

    1. ApiConstants 내용 추가 2. 컨트롤러 프로토타입 작성 - GameTable, TableColumn, UserColumnData

---
+++ src/main/java/com/qwerty/nexus/domain/userData/controller/TableColumnController.java
@@ -0,0 +1,12 @@
+package com.qwerty.nexus.domain.userData.controller;
+
+import com.qwerty.nexus.global.constant.ApiConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiConstants.Path.TABLE_COLUMN_PATH)
+@RequiredArgsConstructor
+public class TableColumnController {
+}
---
+++ src/main/java/com/qwerty/nexus/domain/userData/controller/UserColumnDataController.java
@@ -0,0 +1,14 @@
+package com.qwerty.nexus.domain.userData.controller;
+
+import com.qwerty.nexus.global.constant.ApiConstants;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping(ApiConstants.Path.USER_COLUMN_DATA_PATH)
+@RequiredArgsConstructor
+public class UserColumnDataController {
+}
