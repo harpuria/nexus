074e847bb0b3bfcb93f414886e6011a02e849c12
YHH <harpuria87@gmail.com>
2025-08-18 23:05:48 +0900

    1. Currency, UserCurrency Entity 작성

--- src/main/java/com/qwerty/nexus/domain/gameData/currency/entity/CurrencyEntity.java
+++ src/main/java/com/qwerty/nexus/domain/gameData/currency/entity/CurrencyEntity.java
@@ -3,7 +3,19 @@ package com.qwerty.nexus.domain.gameData.currency.entity;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.OffsetDateTime;
+
 @Getter
 @Builder
 public class CurrencyEntity {
+    private Integer currencyId;
+    private Integer gameId;
+    private String name;
+    private String desc;
+    private Long maxAmount;
+    private OffsetDateTime createdAt;
+    private String createdBy;
+    private OffsetDateTime updatedAt;
+    private String updatedBy;
+    private String isDel;
 }
