5bed1d2a6a97eb5155e879137e90b0b9f2865788
YHH <harpuria87@gmail.com>
2025-09-24 17:18:12 +0900

    - OAuth2 설정은 여기서는 필요없어서 삭제함   모바일 기기에서 소셜로그인 한 뒤에 id_token 백엔드에서 받아서 처리하는 방식으로 하는게 훨씬 나을듯.
    - postgresql localhost 의 경우는 회사에선 15432 포트, 집에서는 5432 포트로 함.

--- src/main/java/com/qwerty/nexus/global/TestAuthController.java
+++ src/main/java/com/qwerty/nexus/global/TestAuthController.java
@@ -22,4 +22,14 @@ public class TestAuthController {
         String token = jwtUtil.generateAccessToken(JwtTokenGenerationData.builder().email(email).build());
         return Map.of("jwt", token);
     }
+
+    @PostMapping("/test-google")
+    public Map<String, String> testGoogle(){
+        // 1) 구글 id_token(얘도 jwt 형식임) 을 받아서 여기서 claims 추출
+        // 2) 추출한 claims 을 통해서 해당 사용자의 정보가 존재하는지 DB 에서 확인
+        // 2-1) 없으면 INSERT 처리
+        // 2-2) 있으면 다음으로 넘어가고
+        // 3) jwt generateAccessToken 진행 후 AccessToken 반환 처리
+        return Map.of();
+    }
 }
