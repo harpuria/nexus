747a2ddaae9757b27e4f72907c819d22f281dfab
HongHun, Yun <harpuria87@gmail.com>
2026-02-04 17:24:29 +0900

    - jackson, jsonb 관련 설정에 주석달아놓음 (안달으니까 까먹어..)

--- src/main/java/com/qwerty/nexus/global/config/jackson/JSONBDeserializer.java
+++ src/main/java/com/qwerty/nexus/global/config/jackson/JSONBDeserializer.java
@@ -7,9 +7,13 @@ import org.jooq.JSONB;
 
 import java.io.IOException;
 
+/**
+ * JSONB 타입을 Java 객체로 변환(역직렬화)하기 위한 커스텀 디시리얼라이저
+ */
 public class JSONBDeserializer extends JsonDeserializer<JSONB> {
     @Override
     public JSONB deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        // JsonNode Parsing 후 JSONB 객체를 반환
         String json = p.readValueAsTree().toString();
         return JSONB.valueOf(json);
     }
--- src/main/java/com/qwerty/nexus/global/config/jackson/JacksonConfig.java
+++ src/main/java/com/qwerty/nexus/global/config/jackson/JacksonConfig.java
@@ -15,10 +15,11 @@ public class JacksonConfig {
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
 
-        // JSONB Deserializer
+        // JSONB Deserializer 등록
         SimpleModule jsonbModule = new SimpleModule();
         jsonbModule.addDeserializer(JSONB.class, new JSONBDeserializer());
 
+        // 날짜,시간을 타임스탬프(숫자형)가 아닌 문자열 형식으로 직렬화 (ex: 2024-02-04T12:34:56)
         builder.modules(jsonbModule, new JavaTimeModule());
         builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
